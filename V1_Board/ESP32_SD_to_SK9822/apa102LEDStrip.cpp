e6e744ba812c73945aa0539f109355a1094e36da
Elec Dash Tron <leon@elec-tron.org>
2022-08-06 10:28:34 +1000

    Port changes for V1 ESP32 Board

---
+++ V1_Board/ESP32_SD_to_SK9822/apa102LEDStrip.cpp
@@ -0,0 +1,32 @@
+#include "apa102LEDStrip.h"
+
+apa102LEDStrip::apa102LEDStrip(short int numLEDs, byte bytesPerLED, byte maxValue, byte globalBrightness)
+{  
+  _numLEDs = numLEDs;
+  _bytesPerLED = bytesPerLED;
+  _maxValue = maxValue;
+  _rainbowSize = maxValue*6;
+  _endFrameLength = 1;//round( (numLEDs/2)/8 );
+  _frameLength = (1+numLEDs+_endFrameLength)*bytesPerLED;
+  _globalBrightness = globalBrightness;
+  LEDs = new byte*[_frameLength];
+  SPIFrame = new byte[_frameLength];
+}
+
+void apa102LEDStrip::renderFrame()
+{
+  for(_index=0; _index<_frameLength; _index++)
+  {
+    SPIFrame[_index] = *LEDs[_index];
+  }
+}
+
+void apa102LEDStrip::setGlobalBrightness(byte globalBrightness)
+{
+  _globalBrightness = globalBrightness;
+  for(_index=_bytesPerLED*2; _index<(_frameLength-(_endFrameLength*_bytesPerLED)); _index+=_bytesPerLED)
+  {
+    LEDs[_index] = &_globalBrightness;
+  }  
+}
+
